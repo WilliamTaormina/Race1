14666e899e4151ef80417af7aff349cb0759d3fe
williamtaormina <williamtaormina@gmail.com>
2018-04-12 13:05:21 -0700

    update app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+let runnerLate = true;
+let runnerAge = 18;
+
+if (runnerAge > 18 && runnerLate == false) {
+  raceNumber += 1000;
+  console.log(`${raceNumber} ${runnerAge} ${runnerLate}`);
+}
+
+if (runnerAge > 18 && runnerLate == false) {
+  raceNumber += 1000;
+  console.log(
+    `You will race at 9:30am today.  Your race number is ${raceNumber}`
+  );
+} else if (runnerAge > 18 && runnerLate == true) {
+  raceNumber += 1000;
+  console.log(
+    `You will race at 11:00am today.  Your race number is ${raceNumber}`
+  );
+} else if (runnerAge < 18) {
+  console.log(
+    `You will race at 12:30am today.  Your race number is ${raceNumber}`
+  );
+} else if (runnerAge === 18) {
+  console.log(`You're ${runnerAge}! Please see the registration desk.`);
+}
